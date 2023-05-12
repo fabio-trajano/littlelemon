0e806814b579bb05b73d5e8e781d37db46415b8c
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-05-12 14:21:46 +0100

    created models

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('menu-items/', views.menu),
+    path('menu-items/', views.MenuItems.as_view()),
+    path('menu-items/<int:pk>', views.Item.as_view()),
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 
 @api_view(['GET','POST'])
 def menu(request):
     return Response('list o items on the menu',
                     status=status.HTTP_200_OK)
+
+class MenuItems(APIView):
+    def get(self, request):
+        return Response({"message": "list o items on the menu"}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        return Response({"message": "new Item added at the menu"}, status=status.HTTP_201_CREATED)
+
+
+class Item(APIView):
+    def get(self, request, pk):
+        return Response({"message": "details of item with id " + str(pk)}, status=status.HTTP_200_OK)
